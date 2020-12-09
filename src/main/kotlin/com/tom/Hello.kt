96718048a01e2acf34eb672976a01a7b3a72e2c9
HaoHsunLi <li880809@gmail.com>
2020-12-09 13:16:18 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main() {
+//  println("hello Kotlin")
+//  Human().hello()
+    val h = Human()
+    h.hello()
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
